b21ec78cab0af80580e7734e7cd2d154657fc6ed
Carlos Mestre <carmesce@gmail.com>
2024-06-19 11:14:45 +0200

    Ejemplos params

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express")
+const calculadoraBackend = express()
+const port = 3000
+
+calculadoraBackend.use(express.static('static/front'))
+
+calculadoraBackend.get("/hola", (req, res) =>{
+    res.send("Hola Calculadora")
+})
+
+const suma = (req, res) => {
+    var sumando1 = Number(req.query.parametro1)
+    var sumando2 = Number(req.query.parametro2)
+    var resultado = sumando1 + sumando2
+    
+    res.send(`SUMA: ${sumando1} + ${sumando2} = ${resultado}`)
+}
+
+calculadoraBackend.get("/calculo", suma)
+
+calculadoraBackend.listen(port, () => {
+    console.log(`Calculadora escuchando en http://localhost:${port}`)
+})
